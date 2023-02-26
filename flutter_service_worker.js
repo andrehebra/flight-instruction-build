129dcf1f9ae6b4520b9603a7016435f0fc0af18a
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-02-25 21:21:20 -0500

    new build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,8 +19,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "211b518298fa2d5684735c592f0fa5ae",
-"/": "211b518298fa2d5684735c592f0fa5ae",
+"index.html": "95e7b4736812634d24a161b668141b6b",
+"/": "95e7b4736812634d24a161b668141b6b",
 "main.dart.js": "156863935164c48d8abf6a7443366466",
 "manifest.json": "3f9f46d43b9a25da68f5dd4800d5ed6d",
 "version.json": "a1a91eabaf2b21a7de7f6d6d8e0ed19d"
